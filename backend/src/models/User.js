8c9cda76816e17bd61f2ab212f79706baa17d62a
htphong01 <thanhphonga4.hbt@gmail.com>
2022-12-14 21:41:06 +0700

    fix model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  socialId: { type: String, unique: true },
+  socialId: { type: String, unique: true, require: true },
   name: { type: String },
   avatar: { type: String },
+  provider: { type: String }
 });
 
 const User = mongoose.model('User', userSchema);
